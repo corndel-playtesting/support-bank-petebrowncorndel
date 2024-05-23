1605ec13b8cb62c4a19f80add738ff7ce0b04e69
petebrowncorndel <peter.brown@corndel.com>
2024-05-23 18:34:03 +0100

    Solves day 3 exercises

--- cli/transaction.js
+++ cli/transaction.js
@@ -6,7 +6,7 @@ transactionController
   .command('log <from> <to> <amount>')
   .description('Log transaction data to the console')
   .action((from, to, amount) => {
-    // TODO
+    console.log(`${from} transferred £${amount} to ${to}.`)
   })
 
 export default transactionController
