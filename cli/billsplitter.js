783f1901ec5c2e0edf209b6768ee9688743b416d
petebrowncorndel <peter.brown@corndel.com>
2024-05-24 13:03:03 +0100

    Solves billSplitter with Bill class

--- cli/billsplitter.js
+++ cli/billsplitter.js
@@ -1,4 +1,5 @@
 import { Command } from 'commander'
+import { Bill } from '../models/bill.js'
 
 const billSplitter = new Command('split')
 
@@ -6,7 +7,8 @@ billSplitter
   .command('log <total> <people>')
   .description('Log ammount to be paid each')
   .action((total, people) => {
-    const priceEach = total / people
+    const bill = new Bill(parseFloat(total))
+    const priceEach = bill.split(parseInt(people))
     console.log(`£${priceEach} should be paid each.`)
   })
 
