7c033babd9040da530889c605605796749492a3e
petebrowncorndel <peter.brown@corndel.com>
2024-05-29 18:47:06 +0100

    Solves day 2 exercise 3

--- exercises/fileIO.js
+++ exercises/fileIO.js
@@ -3,14 +3,18 @@ import fs from 'fs/promises'
 // Part 1: Read the contents of ./resources/1-hello-world.txt
 export async function readHelloWorld() {
   // The path is provided for you:
-  const path = new URL('./resources/1-hello-world.txt', import.meta.url)
-
   // TODO: Return the contents of the file as a utf-8 string
+  const path = new URL('./resources/1-hello-world.txt', import.meta.url)
+  const txt = await fs.readFile(path, 'utf-8')
+  return txt
 }
 
 // Part 2: Return the contents of ./resources/2-diary-entry.txt
 export async function readDiaryEntry() {
   // TODO: Return the contents of the file as a utf-8 string
+  const path = new URL('./resources/2-diary-entry.txt', import.meta.url)
+  const txt = await fs.readFile(path, 'utf-8')
+  return txt
 }
 
 // Part 3: Overwrite ./resources/3-save-data.txt with saveDataString
@@ -22,6 +26,7 @@ export async function writeSaveData(saveData) {
   const saveDataString = JSON.stringify(saveData)
 
   // TODO: Write the saveData string to the file
+  await fs.writeFile(path, saveDataString)
 }
 
 // Part 4: Append text to ./resources/4-log-file.txt
@@ -29,5 +34,8 @@ export async function appendLogEntry(logEntry) {
   // The text to append is given:
   const textToAppend = logEntry + '\n'
 
+  const path = new URL('./resources/4-log-file.txt', import.meta.url)
+
   // TODO: Append the text to the file
+  await fs.appendFile(path, textToAppend)
 }
