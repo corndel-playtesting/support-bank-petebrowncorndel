71129477e1d84aa0cb8bf0d11cfbe3ad6b43567a
Simon Haines <nimon@hotmail.co.uk>
2024-05-29 15:33:37 +0100

    bug fixes and clarifications

--- exercises/validateId.js
+++ exercises/validateId.js
@@ -3,34 +3,30 @@
 export function validateId(id) {
     // Check that the id exists
     if (id === null || id === undefined) {
-        throw new Error('ID is required')
+      throw new Error('ID is required')
     }
-
-    // The id should be a string
+  
+    // The id should be a string:
     if (typeof id !== 'string') {
-        // TODO: Add your code to throw an error here
+      // TODO: Add your code to throw an error here
     }
-
-    // The id should be an odd number of characters long
-    /*
-        TODO: Add your code here
-    */
-
-    // The id should contain the letter 'a'
-    /*
-        TODO: Add your code here
-    */
-
-    // The id should be all lowercase
-    /*
-        TODO: Add your code here
-    */
-}
-
-// Exercise 2 Part 2
-// Use the `validateId` function above to validate the id
-// return true if the id is valid or false otherwise.
-export function isIdValid(id, logger) {
+  
+    // The id should be an odd number of characters long:
+    // TODO: throw an error
+  
+    // The id should contain the letter 'a':
+    // TODO: throw an error
+  
+    // The id should be all lowercase:
+    // TODO: throw an error
+  }
+  
+  // Exercise 2 Part 2
+  // Use the `validateId` function above to validate the id
+  // return true if the id is valid or false otherwise.
+  // If an error is thrown, catch it and call logger.error(err)
+  export function isIdValid(id, logger) {
     logger.error('ID is invalid')
     return null
-}
+  }
+  
