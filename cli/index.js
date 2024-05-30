6874937e2a22408a6adf7b2313077c73d096c7a6
petebrowncorndel <peter.brown@corndel.com>
2024-05-30 16:07:55 +0100

    Workshop update

--- cli/index.js
+++ cli/index.js
@@ -1,10 +1,12 @@
 import { program } from "commander"
-import transactionController from "./transaction.js";
-import billSplitter from "./billsplitter.js";
+import transactionController from "./transaction.js"
+import billSplitter from "./billsplitter.js"
+import listCommand from "./list.js"
 
 program.version('0.1.0').description('SupportBank')
 
 program.addCommand(transactionController)
 program.addCommand(billSplitter)
+program.addCommand(listCommand)
 
 program.parse(process.argv)
